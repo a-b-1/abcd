c79c1456cb7f8a715a84b8103a8ba0b3e97f4045
SONY <SONY@SONY-PC>
2022-03-15 18:42:45 +0530

    changes regarding user stories

---
+++ methods/src/methodsover/Overloading.java
@@ -0,0 +1,17 @@
+package methodsover;
+
+public class Overloading {
+	
+	void demo ()
+		{
+	System.out.println("hello");
+	}
+		void alpha() {
+System.out.println("horney");
+}
+public static void main(String[] args) {
+	Overloading t = new Overloading ();
+	t. alpha ();
+}
+}
+
